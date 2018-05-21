aefde12c81666ccc52128d6212f52cb0258c7824
navy1234 <lihaijun1994@gmail.com>
2018-05-21 18:40:04 +0800

    update

---
+++ MGCarouselCell/MGCarouselCell/MGCarouselCell/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MGCarouselCell
+//
+//  Created by haijun on 2018/5/21.
+//  Copyright © 2018年 wondertex. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
